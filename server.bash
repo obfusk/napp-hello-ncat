a411298e062c7b693f209c1f625226220021bc8e
Felix C. Stegerman <flx@obfusk.net>
2013-08-05 13:42:53 +0200

    read + echo headers

--- server.bash
+++ server.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+while read -r; do
+  x="$( echo "$REPLY" | tr -d $'\r' )"; echo "$x" >&2
+  if [ -z "$x" ]; then break; fi
+done
 cat <<END | sed 's!^  !!'
   HTTP/1.1 200 OK
   Content-Type: text/plain
