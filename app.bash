dc7dda086f99d37b8c9448d60f205eaa8a0d0dee
Felix C. Stegerman <flx@obfusk.net>
2013-08-20 16:40:43 +0200

    fix unkillability

--- app.bash
+++ app.bash
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -e
-p="${1:-${PORT:-8888}}"
-while true; do ncat -l "$p" -e ./server.bash; done
+p="${1:-${PORT:-8888}}" pid=
+cleanup () {
+  set +e
+  echo "bye (pid=$pid)"
+  [ -z "$pid" ] || kill "$pid"
+}
+trap cleanup EXIT
+echo "Listening on $p"
+while true; do
+  ncat -l "$p" -e ./server.bash & pid=$!
+  set +e; wait "$pid"; ok=$? pid=; set -e
+  [ "$ok" == 0 ]
+done
